7ace151298fd524598c724ca4943f4ae66b22e03
shinshinv-dev <shinshinv.dev@gmail.com>
2021-01-15 10:19:49 +0900

    Chapter 02

---
+++ src/test/java/dev/shinshinv/jojoldubook/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package dev.shinshinv.jojoldubook.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_function_test() {
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+}
